9ef3d5dbf621a0815c3cab7a0fb7209e35877e98
DarielBR <dbombinorevuelta@gmail.com>
2023-05-29 17:19:04 +0200

    Main UI elements created.

--- app/src/main/java/com/upmgeoinfo/culturamad/MainActivity.kt
+++ app/src/main/java/com/upmgeoinfo/culturamad/MainActivity.kt
@@ -1,24 +1,247 @@
 package com.upmgeoinfo.culturamad
 
+import android.Manifest
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.CornerSize
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.ExperimentalComposeUiApi
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.platform.LocalLifecycleOwner
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.unit.dp
+import androidx.lifecycle.Lifecycle
+import androidx.lifecycle.LifecycleEventObserver
+import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import com.google.accompanist.permissions.isGranted
+import com.google.accompanist.permissions.rememberMultiplePermissionsState
+import com.google.accompanist.permissions.rememberPermissionState
+import com.google.accompanist.permissions.shouldShowRationale
 import com.upmgeoinfo.culturamad.ui.theme.CulturaMADTheme
 
 class MainActivity : ComponentActivity() {
+
+    @OptIn(ExperimentalPermissionsApi::class)
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             CulturaMADTheme {
-
+                RequestInternetPermission()
+                RequestLocationPermission()
+                UIDeclaration()
             }
+        }
+    }
+}
+/**
+ * Handles proper internet permission requests for a UI built with Compose.
+ * Compose do not have same States as XML layouts apps, therefore is necessary to call
+ * [DisposableEffect] for proper placing in the onCreate state and cleansing after leaving this state.
+ */
+@SuppressLint("PermissionLaunchedDuringComposition")
+@OptIn(ExperimentalPermissionsApi::class)
+@Composable
+fun RequestInternetPermission() {
+    val permissionState =
+        rememberPermissionState(permission = Manifest.permission.INTERNET)
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = lifecycleOwner, effect = {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_START -> {
+                    permissionState.launchPermissionRequest()
+                }
+
+                else -> {}
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    })
+
+    when {
+        permissionState.status.isGranted -> {
+            Text(text = "Reading external permission is granted")
+        }
+        permissionState.status.shouldShowRationale -> {
+            Column {
+                Text(text = "Reading external permission is required by this app")
+            }
+        }
+        !permissionState.status.isGranted && !permissionState.status.shouldShowRationale -> {
+            Text(text = "Permission fully denied. Go to settings to enable")
+        }
+    }
+}
+
+/**
+ * Handles proper location permission requests for a UI built with Compose.
+ * Compose do not have same States as XML layouts apps, therefore is necessary to call
+ * [DisposableEffect] for proper placing in the onCreate state and cleansing after leaving this state.
+ */
+@ExperimentalPermissionsApi
+@Composable
+fun RequestLocationPermission() {
+    val permissionStates = rememberMultiplePermissionsState(
+        permissions = listOf(
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.ACCESS_FINE_LOCATION
+        )
+    )
+    val lifecycleOwner = LocalLifecycleOwner.current
+
+    DisposableEffect(key1 = lifecycleOwner, effect = {
+        val observer = LifecycleEventObserver { _, event ->
+            when (event) {
+                Lifecycle.Event.ON_START -> {
+                    permissionStates.launchMultiplePermissionRequest()
+                }
+
+                else -> {}
+            }
+        }
+        lifecycleOwner.lifecycle.addObserver(observer)
+
+        onDispose {
+            lifecycleOwner.lifecycle.removeObserver(observer)
+        }
+    })
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Top
+    )
+    {
+        permissionStates.permissions.forEach { it ->
+            when (it.permission) {
+                Manifest.permission.READ_EXTERNAL_STORAGE -> {
+                    when {
+                        it.status.isGranted -> {
+                            /* Permission has been granted by the user.
+                               You can use this permission to now acquire the location of the device.
+                               You can perform some other tasks here.
+                            */
+                            Text(text = "Read Ext Storage permission has been granted")
+                        }
+                        it.status.shouldShowRationale -> {
+                            /*Happens if a user denies the permission two times
+
+                             */
+                            Text(text = "Read Ext Storage permission is needed")
+                        }
+                        !it.status.isGranted && !it.status.shouldShowRationale -> {
+                            /* If the permission is denied and the should not show rationale
+                                You can only allow the permission manually through app settings
+                             */
+                            Text(text = "Navigate to settings and enable the Storage permission")
+
+                        }
+                    }
+                }
+                Manifest.permission.ACCESS_FINE_LOCATION -> {
+                    when {
+                        it.status.isGranted -> {
+                            /* Permission has been granted by the user.
+                               You can use this permission to now acquire the location of the device.
+                               You can perform some other tasks here.
+                            */
+                            Text(text = "Location permission has been granted")
+                        }
+                        it.status.shouldShowRationale -> {
+                            /*Happens if a user denies the permission two times
+
+                             */
+                            Text(text = "Location permission is needed")
+
+                        }
+                        !it.status.isGranted && !it.status.shouldShowRationale -> {
+                            /* If the permission is denied and the should not show rationale
+                                You can only allow the permission manually through app settings
+                             */
+                            Text(text = "Navigate to settings and enable the Location permission")
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+/**
+ * Main UI declarative function.
+ */
+@OptIn(ExperimentalComposeUiApi::class, ExperimentalMaterial3Api::class)//Necessary for using [LocalSoftwareKeyboardController.current]. Necessary for using Text Field.
+@Composable
+fun UIDeclaration(
+    modifier: Modifier = Modifier
+){
+    CulturaMADTheme {
+        var searchValue by remember { mutableStateOf("") }
+        val keyboardController = LocalSoftwareKeyboardController.current //necessary to close keyboard after a search is prompted.
+        
+        //MapScreen()
+        
+        Column {
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            /**
+             * Declaring a SearchBar on top of the screen. A Composable function won't be used in
+             * this case because si necessary to modify a variable external to the function's scope.
+             * In particular the searchValue, will be used modified in the following declaration and
+             * it will be used in other task furthermore.
+             */
+            TextField(
+                value = searchValue, 
+                onValueChange = {newText: String ->
+                    searchValue = newText
+                },
+                shape = MaterialTheme.shapes.medium.copy(all = CornerSize(50)),
+                leadingIcon = { Icon(Icons.Filled.Search, contentDescription = null) },
+                placeholder = { Text(stringResource(id = R.string.placeholder_search))},
+                colors = TextFieldDefaults.colors(),
+                keyboardOptions = KeyboardOptions(imeAction = ImeAction.Search),
+                keyboardActions = KeyboardActions( onSearch = { keyboardController?.hide() }),
+                modifier = Modifier
+                    .padding(start = 60.dp, end = 60.dp)
+                    .fillMaxWidth()
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            //CategoryFilters()
         }
     }
 }
---
+++ app/src/main/java/com/upmgeoinfo/culturamad/ui/composables/MapScreen.kt
@@ -0,0 +1,2 @@
+package com.upmgeoinfo.culturamad.ui.composables
+
